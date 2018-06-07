d3a3120c5945414b0e27bb54752190474c1f512b
Mark Dawson <Mark.Dawson@swansea.ac.uk>
2018-06-07 11:46:22 +0100

    remove institution field from project

--- project/forms.py
+++ project/forms.py
@@ -1,8 +1,6 @@
 from django import forms
 from django.db.models import Q
 from django.utils.translation import gettext_lazy as _
-
-from institution.models import Institution
 from project.models import Project
 from project.models import ProjectUserMembership
 
@@ -31,7 +29,7 @@ class ProjectAdminForm(forms.ModelForm):
             'description',
             'legacy_hpcw_id',
             'legacy_arcca_id',
-            'code', 'institution',
+            'code',
             'institution_reference',
             'department',
             'pi',
@@ -109,7 +107,6 @@ class ProjectCreationForm(forms.ModelForm):
             'description',
             'legacy_hpcw_id',
             'legacy_arcca_id',
-            'institution',
             'institution_reference',
             'department',
             'pi',
@@ -134,13 +131,6 @@ class ProjectCreationForm(forms.ModelForm):
                 'class': 'datepicker'
             }),
         }
-
-    def __init__(self, *args, **kwargs):
-        super(ProjectCreationForm, self).__init__(*args, **kwargs)
-        self.fields['institution'] = LocalizeModelChoiceField(
-            queryset=Institution.objects.all(),
-            label=_('Institution'),
-        )
 
 
 class ProjectUserMembershipCreationForm(forms.Form):
